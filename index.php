b5f7fa63fabe311ae7dbb294403d29f561997206
raphaelgyo <raphael.granizo@gmail.com>
2019-12-05 09:55:48 +0100

    correction

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
-  $departments = [02 => 'Aisne', 59 => 'Nord', 60 => 'Oise', 62 => 'Pas-de-calais', 80 => 'Somme'];
+  $departments = array(
+    '02' => 'Aisne',
+    '59' => 'Nord',
+    '60' => 'Oise',
+    '62' => 'Pas-de-calais',
+    '80' => 'Somme');
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <title>PHP Partie 3 Exercice 5 : Les tableaux</title>
   </head>
   <body>
     <?php
-      foreach ($departments as $index => $value) {
-        echo 'Le département' . ' ' . $value . ' ' . 'a le numéro' . ' ' . $index . ' ';
-      }
-    ?>
+      foreach($departments as $index => $value){ ?>
+      <p><?php echo $index . ' ' . $value; ?></p>
+    <?php }; ?>
   </body>
 </html>
